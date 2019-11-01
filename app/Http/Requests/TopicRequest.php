1bbd2af939d7eb610de3aeaa917b9b5df946c83e
vagrant <vagrant@homestead>
2019-11-01 11:46:10 +0000

    用户可以创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -12,7 +12,9 @@ class TopicRequest extends Request
             case 'POST':
             {
                 return [
-                    // CREATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
@@ -20,14 +22,19 @@ class TopicRequest extends Request
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
             case 'DELETE':
             default:
             {
-                return [];
+                return [
+                    'title.min' => '标题必须至少两个字符',
+                    'body.min' => '文章内容必须至少三个字符',
+                ];
             };
         }
     }
